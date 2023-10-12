bcefc4e305a9d482827764340a70eea1da007321
Wilson Chu <wilson1.chu@gmail.com>
2023-10-11 21:18:58 -0400

    add working char counter, changes red when <0

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,15 +1,20 @@
 $(document).ready(function() {
   // any DOM manipulation must happen here
   $("#tweet-text").on("input", function() {
+    const charLimit = 140;
+
     // Get the text from the textarea
     let inputText = $(this).val();
-
-    const charLimit = 140;
     let charCounter = inputText.length;
     let charRemaining = charLimit - charCounter;
 
-    // Changes the characters remaining being shown on screen
-    $('.counter').text(`${charRemaining}`);
+    if (charRemaining < 0) {
+      $('form div output').css('color', 'red');
+    } else {
+      $('form div output').css('color', '#545149');
+    }
+
+    $('form div output').text(`${charRemaining}`);
   });
 
 });
